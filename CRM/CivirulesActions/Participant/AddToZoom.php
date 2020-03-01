5d8ddc2e42c392a93577f6744af700e0afa65aab
Rupert Amodia <rupertraphael@live.com>
2020-02-29 19:16:33 -0700

    wip

--- CRM/CivirulesActions/Participant/AddToZoom.php
+++ CRM/CivirulesActions/Participant/AddToZoom.php
@@ -59,7 +59,7 @@ class CRM_CivirulesActions_Participant_AddToZoom extends CRM_Civirules_Action{
 			$result = civicrm_api3('Contact', 'get', [
 			  'sequential' => 1,
 			  'return' => ["email", "first_name", "last_name", "street_address", "city", "state_province_name", "country", "postal_code"],
-			  'id' => "",
+			  'id' => $id,
 			])['values'][0];
 		} catch (Exception $e) {
 			watchdog(
