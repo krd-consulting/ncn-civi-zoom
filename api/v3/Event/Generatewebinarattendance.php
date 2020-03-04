c04333b219760dfe7f7bcfeb0532857e53a6906f
Rupert Amodia <rupertraphael@live.com>
2020-03-04 13:45:07 -0700

    wip

--- api/v3/Event/Generatewebinarattendance.php
+++ api/v3/Event/Generatewebinarattendance.php
@@ -13,7 +13,7 @@ use Zttp\Zttp;
  *
  * @see https://docs.civicrm.org/dev/en/latest/framework/api-architecture/
  */
-function _civicrm_api3_participant_createfromzoom_spec(&$spec) {
+function _civicrm_api3_participant_generatewebinarattendance_spec(&$spec) {
 	$spec['verification_token']['api.required'] = 1;
 }
 
@@ -31,7 +31,7 @@ function _civicrm_api3_participant_createfromzoom_spec(&$spec) {
  * @see civicrm_api3_create_success
  *
  */
-function civicrm_api3_participant_createfromzoom($params) {
+function civicrm_api3_event_generatewebinarattendance($params) {
 	$verification_token = "H1O4Lg6dQ0Oq3__Xms1qUw";
 
 	$key = $_ENV['ZOOM_API_SECRET'];
@@ -43,11 +43,21 @@ function civicrm_api3_participant_createfromzoom($params) {
 
 	// Get request body
 	$data = json_decode(file_get_contents('php://input'), true);
-	$webinarID = $data['payload']['object']['id'];
+	$webinar = $data['payload']['object']['id'];
+
+	// Get and loop through all of webinar registrants
+	$url = $_ENV['ZOOM_BASE_URL'] . "/past_webinars/$webinar/absentees";
+	$token = $jwt;
+
+	$response = Zttp::withHeaders([
+		'Content-Type' => 'application/json;charset=UTF-8',
+		'Authorization' => "Bearer $token"
+	])->get($url);
+
 
 	return [
 		'values' => [
-			$data['payload']['object']['id']
+			$response->json()
 		]
 	];
 }
