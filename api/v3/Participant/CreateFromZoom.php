82bdf050c5e216897d3c1bf6630a335ab0a15e61
Rupert Amodia <rupertraphael@live.com>
2020-03-04 06:55:38 -0700

    wip createfromzoom.

---
+++ api/v3/Participant/CreateFromZoom.php
@@ -0,0 +1,41 @@
+<?php
+use CRM_Ncnciviapi_ExtensionUtil as E;
+
+
+/**
+ * Contact.FindOrCreate API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ *
+ * @see https://docs.civicrm.org/dev/en/latest/framework/api-architecture/
+ */
+function _civicrm_api3_participant_createfromzoom_spec(&$spec) {
+  // $spec['contact_type']['api.required'] = 1;
+  // $spec['first_name']['api.required'] = 1;
+  // $spec['last_name']['api.required'] = 1;
+  // $spec['email']['api.required'] = 1;
+}
+
+/**
+ * Contact.FindOrCreate API
+ *
+ * Queries for an individual using first_name, last_name, and email and creates a contact if
+ * no contacts are returned. 
+ *
+ * @param array $params
+ *
+ * @return array
+ *   Array containing data of found or newly created contact.
+ *
+ * @see civicrm_api3_create_success
+ *
+ */
+function civicrm_api3_participant_createfromzoom($params) {
+
+	return [
+		'values' => [
+			$params
+		]
+	];
+}
